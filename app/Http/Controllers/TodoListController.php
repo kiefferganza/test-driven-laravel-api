56f8adf6a612e30f7b1574470a10985dc3650038
Kieffer <kganza@traxiontech.net>
2022-12-02 10:39:56 +0800

    unit test for fetching all,single and storing todos

---
+++ app/Http/Controllers/TodoListController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TodoList;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TodoListController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $lists = TodoList::all();
+        return response($lists);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $list = TodoList::create($request->all());
+        return response($list, Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TodoList $list)
+    {
+        return response($list);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ tests/Feature/TodoListTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\TodoList;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TodoListTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private mixed $list;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->list = TodoList::factory()->create();
+    }
+
+    public function test_fetch_all_todo_list()
+    {
+        $response = $this->getJson(route('todo-list.index'));
+
+        $this->assertCount(1, $response->json());
+    }
+
+    public function test_fetch_single_todo_List()
+    {
+        $response = $this->getJson(route('todo-list.show', $this->list->id))
+            ->assertOk()
+            ->json();
+
+        $this->assertEquals($response['name'], $this->list->name);
+    }
+
+    public function test_store_new_todo_list()
+    {
+        $list = ['name' => 'test list'];
+        $response = $this->postJson(route('todo-list.store'), $list)
+            ->assertCreated()
+            ->json();
+
+        $this->assertEquals('test list', $response['name']);
+        $this->assertDatabaseHas('todo_lists', $list);
+    }
+}
